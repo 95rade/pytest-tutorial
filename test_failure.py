fda4d604727f02086abebc9cd977201a5279d5b4
rmilij001c <rade95@gmail.com>
2022-10-12 07:30:36 -0600

    added a cc pipeline

--- test_failure.py
+++ test_failure.py
@@ -2,7 +2,10 @@ import pytest
 import math
 
 def test_sqrt_failure():
-    num=25
+    '''
+    num = 6
+    '''
+    num = input('Enter number ')
     assert math.sqrt(num) == 6
 def test_square_failure():
     num=7
