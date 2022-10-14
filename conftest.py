731acfc788f97530e0290db66b51b490d5ce70f3
rmilij001c <rade95@gmail.com>
2022-10-14 09:25:28 -0600

    minor fixes

--- conftest.py
+++ conftest.py
@@ -2,7 +2,7 @@ import pytest
 
 @pytest.fixture
 def input_value():
-    input = 36
+    input = 39
     #input = int(input('Enter number:\n'))
     """ pytest -s would invoke user input """
     return input
